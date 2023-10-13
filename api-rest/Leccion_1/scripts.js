51f7d5cb34c881603bb9e93eab3d6bd1b1b0a500
ciit4ivadeveloper <127860965+ciit4ivadeveloper@users.noreply.github.com>
2023-10-12 21:46:03 -0500

    Practica en clase

---
+++ api-rest/Leccion_1/scripts.js
@@ -0,0 +1,55 @@
+const API = 'https://swapi.dev/api/people/'
+
+function personaje(texto) { 
+    let div = document.createElement('div')
+    let h1_texto = document.createTextNode(texto.url+" ->> "+texto.name) 
+    div.appendChild(h1_texto)
+     
+
+
+    var table = document.createElement('table');
+    var tr = document.createElement('tr');
+    var tdUrl = document.createElement('td');  
+    var tdName = document.createElement('td');    
+
+    tdUrl.style.border = '1px solid #000';
+    tdName.style.border = '1px solid #000';
+    tdUrl.textContent = texto.url;
+    tdName.textContent = texto.name; 
+   
+    tr.appendChild(tdUrl);
+    tr.appendChild(tdName); 
+    table.appendChild(tr);
+
+
+   /* let img = document.createElement('img')
+    img.src = texto.image
+    div.appendChild(img)*/
+
+    let contenedor = document.getElementById('contenedor')
+    contenedor.appendChild( table )
+}
+
+function obtener_personaje(id) {
+    return new Promise((resolve, reject) => {
+        fetch(`${API}${id}`)
+            .then((data) => resolve(data.json()))
+            .catch((error) => reject(`[error]: ${error}`))
+    })
+}
+
+let ids = []
+for (let i=1; i<=16; i++) {
+    ids.push(i)
+}
+
+let promesas = ids.map( id => obtener_personaje(id) )
+
+// Promesas Encadenadas
+Promise
+    .all( promesas )
+    .then( data => {
+        for (let i=0; i<data.length; i++) {
+            personaje( data[i] )
+        }
+    } )
